21749bb5a8a2950b05359789ae989a4d3d5f32d6
Emon <150001421+doodleEmon@users.noreply.github.com>
2025-01-02 20:43:40 +0600

    tic-tac-toe done

---
+++ src/components/Board.jsx
@@ -0,0 +1,55 @@
+import { useState } from "react";
+import Square from "./Square";
+import useCalculateWinner from "../hooks/useCalculateWinner";
+
+const Board = () => {
+  const [xIsNext, setXIsNext] = useState(true);
+  const [squares, setSquares] = useState(Array(9).fill(null));
+
+  const handleClick = (i) => {
+    const nextSquares = squares.slice();
+
+    if (squares[i] || useCalculateWinner(squares)) {
+      return; // if it is true, then the execution will be stop here and go out from this handleClick function, won't go through down!
+    }
+
+    if (xIsNext) {
+      nextSquares[i] = "X";
+    } else {
+      nextSquares[i] = "O";
+    }
+    setSquares(nextSquares);
+    setXIsNext(!xIsNext);
+  };
+
+  const winner = useCalculateWinner(squares);
+  let status;
+  if (winner) {
+    status = `Winner is ${winner}`;
+  } else {
+    status = `Player turn: ${xIsNext ? "X" : "O"}`;
+  }
+
+  return (
+    <div className="w-36">
+      <h3 className="text-red-500 text-lg font-semibold my-3">{status}</h3>
+      <div className="board-row">
+        <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
+        <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
+        <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
+      </div>
+      <div className="board-row">
+        <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
+        <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
+        <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
+      </div>
+      <div className="board-row">
+        <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
+        <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
+        <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
+      </div>
+    </div>
+  );
+};
+
+export default Board;
